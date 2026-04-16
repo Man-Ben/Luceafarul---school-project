0c99ec4b26f50409abb79c4108442897d362151e
MBen <mabeniamin15@gmail.com>
2026-04-16 23:14:46 +0300

    Features: Background moves, player controllable. Refactor: add rb + formode to bg manager

--- Mechanics Scripts/BackGroundManager.cs
+++ Mechanics Scripts/BackGroundManager.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
 
-public class BackGroundManager : MonoBehaviour
+public class BackgroundManager : MonoBehaviour
 {
+
+    [SerializeField] float backGroundSpeed = 10;
+
+    Vector2 ResetPosition = new Vector2(107.6f, 0);
+
+    void Update()
+    {
+        Moving();
+    }
+
+    void Moving()
+    {
+        transform.Translate(Vector2.left * backGroundSpeed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("BackgroundBound"))
+            transform.position = ResetPosition;
+    }
 
 }
--- Mechanics Scripts/PlayerController.cs
+++ Mechanics Scripts/PlayerController.cs
@@ -2,5 +2,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] float ascendForce = 5;
+    [SerializeField] float gravityModifier = 2;
+
+    Rigidbody2D playerRb;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+
+        Physics.gravity *= gravityModifier;
+    }
+
+    void FixedUpdate()
+    {
+        Fly();
+    }
+
+    void Fly()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            playerRb.AddForce(Vector2.up * ascendForce, ForceMode2D.Impulse);
+        }
+
+    }
 
 }
